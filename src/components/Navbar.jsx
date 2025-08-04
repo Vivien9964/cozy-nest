39a54b39f5b77c2e872e3456a2eb356710c3bc47
Vivien99964 <mecserikvivien2@gmail.com>
2025-08-04 11:58:21 +0300

    Created base structure of the application and set up routing between pages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,13 +7,11 @@ const Navbar = () => {
     <div>
         <nav>
         <ul>
-
             <li><Link to={'/'}>Home</Link></li>
             <li><Link to={'/moodboard'}>Mood Board</Link></li>
             <li><Link to={'/planner'}>Cozy Planner</Link></li>
             <li><Link to={'/recipes'}>Fall Recipes</Link></li>
             <li><Link to={'/countdown'}>Events</Link></li>
-    
         </ul>
         </nav>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <h1>Welcome to Coyz Nest!</h1>
+      <p>Take a look at my autumn-themed small projects, which I've built to practice and learn React!</p>
+    </div>
   )
 }
 
